df20234cf7aca2d17cd646060193eb626840487c
vietanhle97 <vietanhle0710@gmail.com>
2020-05-31 03:27:42 +0700

    LC

---
+++ Course_Schedule_207.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func dfsCycle(m map[int][]int, visited []bool, rec []bool, v int) bool {
+	visited[v] = true
+	rec[v] = true
+	for _, e := range m[v] {
+		if visited[e] == false {
+			if dfsCycle(m, visited, rec, e) {
+				return true
+			}
+		}
+		if rec[e] {
+			return true
+		}
+	}
+	rec[v] = false
+	return false
+}
+
+func canFinish(numCourses int, prerequisites [][]int) bool {
+	m := map[int][]int{}
+	for _, e := range prerequisites {
+		_, ok := m[e[0]]
+		if ok {
+			m[e[0]] = append(m[e[0]], e[1])
+		} else {
+			m[e[0]] = []int{e[1]}
+		}
+	}
+	visited := make([]bool, numCourses)
+	rec := make([]bool, numCourses)
+	for k, _ := range m {
+		if visited[k] == false {
+			if dfsCycle(m, visited, rec, k) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func main() {
+	p := [][]int{{1, 0}, {0, 1}, {2, 3}, {2, 1}}
+	fmt.Println(canFinish(4, p))
+}
